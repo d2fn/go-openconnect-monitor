130ffa3d373a0a78392cab217531f8d04dd2598f
Dietrich Featherston <d@d2fn.com>
2025-11-02 00:37:13 -0500

    add btop config showing tun0 activity in tmux pane

--- launch.sh
+++ launch.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-tmux new-session -d -s openconnect-pulse -n openconnect 'nix run .#poller'
-tmux split-window -v -t openconnect-pulse:0 'sudo nix run .#openconnect'
-tmux attach -t openconnect-pulse
+session_name="openconnect-pulse"
+
+tmux new-session -d -s $session_name -n openconnect 'btop -c btop.config'
+tmux split-window -v -t $session_name:0 'nix run .#poller'
+tmux split-window -v -t $session_name:0 'sudo nix run .#openconnect'
+tmux attach -t $session_name
 
