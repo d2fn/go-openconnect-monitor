143322a0ea4792d0ec0a63a5bf5a05826a2bce0d
Dietrich Featherston <d@d2fn.com>
2025-12-08 16:00:01 -0800

    formatting

--- flake.nix
+++ flake.nix
@@ -1,23 +1,23 @@
 {
   description = "Go app with a locally patched openconnect";
 
-	inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, ... }:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-			inherit system;
-		};
-  in {
-    # HM module
-    homeManagerModules.vpnManager = ./modules/hm-vpn-manager.nix;
-    nixosModules.vpnManager = ./modules/nixos-vpn-manager.nix;
-    # Package
-    packages.${system} = {
-			vpnManager = pkgs.callPackage ./pkgs/vpn-manager.nix { };
-			default = self.packages.${system}.vpnManager;
-		};
-  };
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      homeManagerModules.vpnManager = ./modules/hm-vpn-manager.nix;
+      nixosModules.vpnManager = ./modules/nixos-vpn-manager.nix;
+      # Package
+      packages.${system} = {
+        vpnManager = pkgs.callPackage ./pkgs/vpn-manager.nix { };
+        default = self.packages.${system}.vpnManager;
+      };
+    };
 }
-
